bb28d95dcbb951ece719455de89edf31d280a0aa
Abraham Flaxman <abie@uw.edu>
2020-06-27 16:48:20 -0700

    TST: better validation and automatic testing for beta_finalize

--- src/vivarium_uw_covid/components/seiir_beta.py
+++ src/vivarium_uw_covid/components/seiir_beta.py
@@ -6,7 +6,7 @@ Date: 2020-06-25
 """
 
 import numpy as np, pandas as pd
-from typing import Tuple, Union
+from typing import Tuple, Union, Dict
 
 def beta_predict(coeffs, covs):
     """Predict beta(t) values from coefficients
@@ -37,38 +37,50 @@ def beta_predict(coeffs, covs):
     return pd.DataFrame(np.exp(log_beta), index=covs.index)
 
 
-# from https://github.com/ihmeuw/covid-model-seiir-pipeline/blob/master/src/covid_model_seiir_pipeline/core/utils.py#L128
+# from https://github.com/ihmeuw/covid-model-seiir-pipeline/blob/master/src/covid_model_seiir_pipeline/core/utils.py
 def beta_shift(beta_fit: pd.DataFrame,
                beta_pred: np.ndarray,
+               draw_id: int,
                window_size: Union[int, None] = None,
-               avg_over: Union[int, None] = None) -> Tuple[np.ndarray, float]:
+               average_over_min: int = 1,
+               average_over_max: int = 35) -> Tuple[np.ndarray, Dict[str, float]]:
     """Calculate the beta shift.
     Args:
         beta_fit (pd.DataFrame): Data frame contains the date and beta fit.
         beta_pred (np.ndarray): beta prediction.
-        windown_size (Union[int, None], optional):
+        draw_id (int): Draw of data provided.  Will be used as a seed for
+            a random number generator to determine the amount of beta history
+            to leverage in rescaling the y-intercept for the beta prediction.
+        window_size (Union[int, None], optional):
             Window size for the transition. If `None`, Hard shift no transition.
             Default to None.
-        avg_over (Union[int, None], optional):
-            Final beta scale depends on the ratio between beta prediction over the
-            average beta over `avg_over` days. If `None`, final scale will be 1, means
-            return to the `beta_pred` completely. Default to None.
     Returns:
         Tuple[np.ndarray, float]: Predicted beta, after scaling (shift) and the initial scaling.
     """
     assert 'date' in beta_fit.columns, "'date' has to be in beta_fit data frame."
     assert 'beta' in beta_fit.columns, "'beta' has to be in beta_fit data frame."
     beta_fit = beta_fit.sort_values('date')
+    beta_hat = beta_fit['beta_pred'].values
     beta_fit = beta_fit['beta'].values
 
-    anchor_beta = beta_fit[-1]
-    scale_init = anchor_beta / beta_pred[0]
+    rs = np.random.RandomState(seed=draw_id)
+    avg_over = rs.randint(average_over_min, average_over_max)
 
-    if avg_over is None:
-        scale_final = 1.0
-    else:
-        beta_history = beta_fit[-avg_over:]
-        scale_final = beta_history.mean() / beta_pred[0]
+    beta_fit_final = beta_fit[-1]
+    beta_pred_start = beta_pred[0]
+
+    scale_init = beta_fit_final / beta_pred_start
+    log_beta_resid = np.log(beta_fit / beta_hat)
+    scale_final = np.exp(log_beta_resid[-avg_over:].mean())
+
+    scale_params = {
+        'window_size': window_size,
+        'history_days': avg_over,
+        'fit_final': beta_fit_final,
+        'pred_start': beta_pred_start,
+        'beta_ratio_mean': scale_final,
+        'beta_residual_mean': np.log(scale_final),
+    }
 
     if window_size is not None:
         assert isinstance(window_size, int) and window_size > 0, f"window_size={window_size} has to be a positive " \
@@ -80,7 +92,7 @@ def beta_shift(beta_fit: pd.DataFrame,
 
     betas = beta_pred * scale
 
-    return betas, scale_init
+    return betas, scale_params
 
 
 def beta_finalize(beta_pred, beta_fit):
@@ -98,14 +110,19 @@ def beta_finalize(beta_pred, beta_fit):
     returns pd.DataFrame of final beta values, with columns for each draw,
     and rows for each day
     """
+    assert len(beta_pred.index.unique()) == len(beta_pred.index), 'expect no duplicate values in index of beta_pred'
     beta_final = pd.DataFrame(columns=beta_pred.columns, index=beta_pred.index)
     crossover_time = pd.Timestamp(beta_fit[0].date.max())
     for draw in beta_fit.keys():
-        s_final, scale_init = beta_shift(beta_fit=beta_fit[draw],
-                                         beta_pred=beta_pred.loc[crossover_time:, draw],
-                                         window_size=28,
-                                         avg_over=21)
-        beta_final.loc[:, draw] = s_final
+        s_final, scale_params = beta_shift(
+            beta_fit=beta_fit[draw],
+            beta_pred=beta_pred.loc[crossover_time:, draw].values, # Peng likes numpy arrays
+            draw_id=draw,  # Assuming draw is a string like 'draw_335'
+            window_size=28,
+            average_over_min=1,
+            average_over_max=21
+        )
+        beta_final.loc[crossover_time:, draw] = s_final
     return beta_final
 
 
