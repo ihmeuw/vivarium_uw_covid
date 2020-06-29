59546fc301254aeeebf8b8da101223a5e8c9ee27
Abraham Flaxman <abie@uw.edu>
2020-06-29 15:29:20 -0700

    WIP: mechanistic testing in agent step

--- src/vivarium_uw_covid/components/seiir_agent.py
+++ src/vivarium_uw_covid/components/seiir_agent.py
@@ -34,7 +34,7 @@ def agent_covid_step(df, alpha, beta, gamma1, gamma2, sigma, theta):
 
     return agent_covid_step_with_infection_rate(df, infection_rate, alpha, gamma1, gamma2, sigma, theta)
 
-def agent_covid_step_with_infection_rate(df, infection_rate, alpha, gamma1, gamma2, sigma, theta):
+def agent_covid_step_with_infection_rate(df, infection_rate, alpha, gamma1, gamma2, sigma, theta, testing_rate):
     uniform_random_draw = np.random.uniform(size=len(df))
 
     # update from R back to S, to allow in-place computation
@@ -53,7 +53,24 @@ def agent_covid_step_with_infection_rate(df, infection_rate, alpha, gamma1, gamm
     pr_S_to_E = 1 - np.exp(-infection_rate)
     rows = (df.covid_state == 'S') & (uniform_random_draw < pr_S_to_E)
     df.loc[rows, 'covid_state'] = 'E'
-    return np.sum(rows) # new_infections
+    new_infections = np.sum(rows)
+
+    #### experimental code for mechanistic testing-and-isolation model
+    test_positive_rate = 0.05
+    n_test_positive = testing_rate * test_positive_rate * len(df)
+    n_infected = ((df.covid_state == 'E') | (df.covid_state == 'I1') | (df.covid_state == 'I2')).sum()
+
+    testing_rate_among_infected = n_test_positive / (n_infected + 1)  # HACK: add 1 to denominator to avoid divide by zero
+    if testing_rate_among_infected > 10:
+        pr_tested = 1
+    else:
+        pr_tested = 1 - np.exp(-testing_rate_among_infected)
+    #pr_tested = 1 - np.exp(-testing_rate)
+    rows = (df.covid_state != 'S') & (np.random.uniform(size=len(df)) < pr_tested)  # FIXME: detected too soon?
+    df.loc[rows, 'covid_state'] = 'R'  # move any non-S state individual to state R if they are tested (FIXME: too simple)
+
+
+    return new_infections
 
 
 def run_agent_model(n_draws, n_simulants, params, beta, start_time, initial_states):
